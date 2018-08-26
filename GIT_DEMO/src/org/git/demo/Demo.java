08d689a0553eb38fd3dfc668e8191adbaeb87b32
Sachin Thakur <E01576@hsc.com>
2018-08-26 13:56:13 +0530

    one

--- GIT_DEMO/src/org/git/demo/Demo.java
+++ GIT_DEMO/src/org/git/demo/Demo.java
@@ -5,8 +5,6 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("My name is sachin thakur");
-		System.out.println("What is your address");
-		System.out.println("feature_t cwohange");
+		
 	}
 }
